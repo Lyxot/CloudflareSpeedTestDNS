14ec6b97887440d47cbc6f1c196f2a024a46bee7
Lyxot <1169942595@qq.com>
2025-08-27 23:44:13 +0800

    remove cli args

--- config.go
+++ config.go
@@ -11,6 +11,55 @@ import (
 	"github.com/Lyxot/CloudflareSpeedTestDNS/utils"
 )
 
+// Config 配置文件结构体
+type Config struct {
+	// 延迟测速相关
+	Routines    int     `toml:"routines"`      // 延迟测速线程
+	PingTimes   int     `toml:"ping_times"`    // 延迟测速次数
+	TCPPort     int     `toml:"tcp_port"`      // 指定测速端口
+	MaxDelay    int     `toml:"max_delay"`     // 平均延迟上限
+	MinDelay    int     `toml:"min_delay"`     // 平均延迟下限
+	MaxLossRate float64 `toml:"max_loss_rate"` // 丢包几率上限
+
+	// HTTP测速相关
+	Httping     bool   `toml:"httping"`      // 切换测速模式为HTTP
+	HttpingCode int    `toml:"httping_code"` // 有效状态代码
+	CFColo      string `toml:"cfcolo"`       // 匹配指定地区
+
+	// 下载测速相关
+	TestCount    int     `toml:"test_count"`       // 下载测速数量
+	DownloadTime int     `toml:"download_time"`    // 下载测速时间
+	URL          string  `toml:"url"`              // 指定测速地址
+	MinSpeed     float64 `toml:"min_speed"`        // 下载速度下限
+	Disable      bool    `toml:"disable_download"` // 禁用下载测速
+
+	// 输入输出相关
+	PrintNum int    `toml:"print_num"` // 显示结果数量
+	IPFile   string `toml:"ip_file"`   // IP段数据文件
+	IPv4File string `toml:"ipv4_file"` // IPv4段数据文件
+	IPv6File string `toml:"ipv6_file"` // IPv6段数据文件
+	IPText   string `toml:"ip_text"`   // 指定IP段数据
+	Output   string `toml:"output"`    // 输出结果文件
+
+	// 其他选项
+	TestAll bool `toml:"test_all"` // 测速全部IP
+
+	// 阿里云DNS相关
+	AliDNS AliDNSConfig `toml:"alidns"` // 阿里云DNS配置
+
+	// DNSPod DNS相关
+	DNSPod DNSPodConfig `toml:"dnspod"` // DNSPod DNS配置
+
+	// Cloudflare DNS相关
+	Cloudflare CloudflareConfig `toml:"cloudflare"` // Cloudflare DNS配置
+
+	// Cloudflare KV相关
+	CFKV CloudflareKVConfig `toml:"cfkv"` // Cloudflare KV配置
+
+	// Cron 定时任务相关
+	Cron CronConfig `toml:"cron"`
+}
+
 // AliDNSConfig 阿里云DNS配置
 type AliDNSConfig struct {
 	Enable          bool   `toml:"enable"`           // 是否启用阿里云DNS
@@ -59,56 +108,6 @@ type CronConfig struct {
 	TestInterval      int     `toml:"test_interval"`
 }
 
-// Config 配置文件结构体
-type Config struct {
-	// 延迟测速相关
-	Routines    int     `toml:"routines"`      // 延迟测速线程
-	PingTimes   int     `toml:"ping_times"`    // 延迟测速次数
-	TCPPort     int     `toml:"tcp_port"`      // 指定测速端口
-	MaxDelay    int     `toml:"max_delay"`     // 平均延迟上限
-	MinDelay    int     `toml:"min_delay"`     // 平均延迟下限
-	MaxLossRate float64 `toml:"max_loss_rate"` // 丢包几率上限
-
-	// HTTP测速相关
-	Httping     bool   `toml:"httping"`      // 切换测速模式为HTTP
-	HttpingCode int    `toml:"httping_code"` // 有效状态代码
-	CFColo      string `toml:"cfcolo"`       // 匹配指定地区
-
-	// 下载测速相关
-	TestCount    int     `toml:"test_count"`       // 下载测速数量
-	DownloadTime int     `toml:"download_time"`    // 下载测速时间
-	URL          string  `toml:"url"`              // 指定测速地址
-	MinSpeed     float64 `toml:"min_speed"`        // 下载速度下限
-	Disable      bool    `toml:"disable_download"` // 禁用下载测速
-
-	// 输入输出相关
-	PrintNum int    `toml:"print_num"` // 显示结果数量
-	IPFile   string `toml:"ip_file"`   // IP段数据文件
-	IPv4File string `toml:"ipv4_file"` // IPv4段数据文件
-	IPv6File string `toml:"ipv6_file"` // IPv6段数据文件
-	IPText   string `toml:"ip_text"`   // 指定IP段数据
-	Output   string `toml:"output"`    // 输出结果文件
-
-	// 其他选项
-	TestAll bool `toml:"test_all"` // 测速全部IP
-	Debug   bool `toml:"debug"`    // 调试输出模式
-
-	// 阿里云DNS相关
-	AliDNS AliDNSConfig `toml:"alidns"` // 阿里云DNS配置
-
-	// DNSPod DNS相关
-	DNSPod DNSPodConfig `toml:"dnspod"` // DNSPod DNS配置
-
-	// Cloudflare DNS相关
-	Cloudflare CloudflareConfig `toml:"cloudflare"` // Cloudflare DNS配置
-
-	// Cloudflare KV相关
-	CFKV CloudflareKVConfig `toml:"cfkv"` // Cloudflare KV配置
-
-	// Cron 定时任务相关
-	Cron CronConfig `toml:"cron"`
-}
-
 // LoadConfig 从TOML文件加载配置
 func LoadConfig(path string) (*Config, error) {
 	config := &Config{}
@@ -126,6 +125,56 @@ func LoadConfig(path string) (*Config, error) {
 	}
 
 	return config, nil
+}
+
+// CreateDefaultConfig 创建默认配置
+func CreateDefaultConfig() *Config {
+	return &Config{
+		Routines:     200,
+		PingTimes:    4,
+		TCPPort:      443,
+		MaxDelay:     9999,
+		MinDelay:     0,
+		MaxLossRate:  1.0,
+		Httping:      false,
+		HttpingCode:  0,
+		CFColo:       "",
+		TestCount:    10,
+		DownloadTime: 10,
+		URL:          "https://cf.xiu2.xyz/url",
+		MinSpeed:     0.0,
+		Disable:      false,
+		PrintNum:     10,
+		IPFile:       "ip.txt",
+		IPv4File:     "",
+		IPv6File:     "",
+		IPText:       "",
+		Output:       "result.csv",
+		TestAll:      false,
+		AliDNS: AliDNSConfig{
+			Enable: false,
+			TTL:    600,
+		},
+		DNSPod: DNSPodConfig{
+			Enable: false,
+			TTL:    600,
+		},
+		Cloudflare: CloudflareConfig{
+			Enable:  false,
+			Proxied: false,
+			TTL:     1,
+		},
+		CFKV: CloudflareKVConfig{
+			Enable: false,
+		},
+		Cron: CronConfig{
+			Enable:            false,
+			LatencyThreshold:  9999,
+			LossRateThreshold: 1.0,
+			CheckInterval:     30,
+			TestInterval:      24,
+		},
+	}
 }
 
 // ApplyConfig 应用配置到全局变量
@@ -201,30 +250,23 @@ func ApplyConfig(config *Config) {
 	task.TestAll = config.TestAll
 
 	// 设置阿里云DNS相关参数
+	enableAliDNS = config.AliDNS.Enable
 	ddns.AliDNSConfig.AccessKeyID = config.AliDNS.AccessKeyID
 	ddns.AliDNSConfig.AccessKeySecret = config.AliDNS.AccessKeySecret
 	ddns.AliDNSConfig.Domain = config.AliDNS.Domain
 	ddns.AliDNSConfig.Subdomain = config.AliDNS.Subdomain
 	ddns.AliDNSConfig.TTL = config.AliDNS.TTL
 
-	// 如果配置文件中启用了阿里云DNS，则覆盖命令行参数
-	if config.AliDNS.Enable {
-		enableAliDNS = true
-	}
-
 	// 设置DNSPod DNS相关参数
+	enableDNSPod = config.DNSPod.Enable
 	ddns.DNSPodConfig.SecretID = config.DNSPod.SecretID
 	ddns.DNSPodConfig.SecretKey = config.DNSPod.SecretKey
 	ddns.DNSPodConfig.Domain = config.DNSPod.Domain
 	ddns.DNSPodConfig.Subdomain = config.DNSPod.Subdomain
 	ddns.DNSPodConfig.TTL = config.DNSPod.TTL
 
-	// 如果配置文件中启用了DNSPod DNS，则覆盖命令行参数
-	if config.DNSPod.Enable {
-		enableDNSPod = true
-	}
-
 	// 设置Cloudflare DNS相关参数
+	enableCloudflare = config.Cloudflare.Enable
 	ddns.CloudflareConfig.APIToken = config.Cloudflare.APIToken
 	ddns.CloudflareConfig.ZoneID = config.Cloudflare.ZoneID
 	ddns.CloudflareConfig.Domain = config.Cloudflare.Domain
@@ -232,28 +274,15 @@ func ApplyConfig(config *Config) {
 	ddns.CloudflareConfig.Proxied = config.Cloudflare.Proxied
 	ddns.CloudflareConfig.TTL = config.Cloudflare.TTL
 
-	// 如果配置文件中启用了Cloudflare DNS，则覆盖命令行参数
-	if config.Cloudflare.Enable {
-		enableCloudflare = true
-	}
-
 	// 设置Cloudflare KV相关参数
+	enableCFKV = config.CFKV.Enable
 	ddns.CloudflareKVConfig.APIToken = config.CFKV.APIToken
 	ddns.CloudflareKVConfig.AccountID = config.CFKV.AccountID
 	ddns.CloudflareKVConfig.NamespaceID = config.CFKV.NamespaceID
 
-	// 如果配置文件中启用了Cloudflare KV，则覆盖命令行参数
-	if config.CFKV.Enable {
-		enableCFKV = true
-	}
-
 	// 设置输入输出相关参数
 	if config.PrintNum >= 0 {
 		utils.PrintNum = config.PrintNum
-	}
-
-	if config.Debug {
-		utils.Debug = config.Debug
 	}
 
 	if config.Output != "" {
--- main.go
+++ main.go
@@ -37,205 +37,45 @@ CloudflareSpeedTestDNS ` + version + `
 https://github.com/Lyxot/CloudflareSpeedTestDNS
 
 参数：
-    -n 200
-        延迟测速线程；越多延迟测速越快，性能弱的设备 (如路由器) 请勿太高；(默认 200 最多 1000)
-    -t 4
-        延迟测速次数；单个 IP 延迟测速的次数；(默认 4 次)
-    -dn 10
-        下载测速数量；延迟测速并排序后，从最低延迟起下载测速的数量；(默认 10 个)
-    -dt 10
-        下载测速时间；单个 IP 下载测速最长时间，不能太短；(默认 10 秒)
-    -tp 443
-        指定测速端口；延迟测速/下载测速时使用的端口；(默认 443 端口)
-    -url https://cf.xiu2.xyz/url
-        指定测速地址；延迟测速(HTTPing)/下载测速时使用的地址，默认地址不保证可用性，建议自建；
-
-    -httping
-        切换测速模式；延迟测速模式改为 HTTP 协议，所用测试地址为 [-url] 参数；(默认 TCPing)
-    -httping-code 200
-        有效状态代码；HTTPing 延迟测速时网页返回的有效 HTTP 状态码，仅限一个；(默认 200 301 302)
-    -cfcolo HKG,KHH,NRT,LAX,SEA,SJC,FRA,MAD
-        匹配指定地区；IATA 机场地区码或国家/城市码，英文逗号分隔，仅 HTTPing 模式可用；(默认 所有地区)
-
-    -tl 200
-        平均延迟上限；只输出低于指定平均延迟的 IP，各上下限条件可搭配使用；(默认 9999 ms)
-    -tll 40
-        平均延迟下限；只输出高于指定平均延迟的 IP；(默认 0 ms)
-    -tlr 0.2
-        丢包几率上限；只输出低于/等于指定丢包率的 IP，范围 0.00~1.00，0 过滤掉任何丢包的 IP；(默认 1.00)
-    -sl 5
-        下载速度下限；只输出高于指定下载速度的 IP，凑够指定数量 [-dn] 才会停止测速；(默认 0.00 MB/s)
-
-    -p 10
-        显示结果数量；测速后直接显示指定数量的结果，为 0 时不显示结果直接退出；(默认 10 个)
-    -f4 ipv4.txt
-        IPv4段数据文件；如路径含有空格请加上引号；支持其他 CDN IP段；(默认为空)
-    -f6 ipv6.txt
-        IPv6段数据文件；如路径含有空格请加上引号；支持其他 CDN IP段；(默认为空)
-    -f ip.txt
-        IP段数据文件；如路径含有空格请加上引号；支持其他 CDN IP段；(默认 ip.txt，指定f4或f6时该参数无效)
-    -ip 1.1.1.1,2.2.2.2/24,2606:4700::/32
-        指定IP段数据；直接通过参数指定要测速的 IP 段数据，英文逗号分隔；(默认 空)
-    -o result.csv
-        写入结果文件；如路径含有空格请加上引号；值为空时不写入文件 [-o ""]；(默认 result.csv)
-
-    -dd
-        禁用下载测速；禁用后测速结果会按延迟排序 (默认按下载速度排序)；(默认 启用)
-    -allip
-        测速全部的IP；对 IP 段中的每个 IP (仅支持 IPv4) 进行测速；(默认 每个 /24 段随机测速一个 IP)
-
-    -alidns
-        启用阿里云DNS更新；测速完成后将结果更新到阿里云DNS；(默认 关闭)
-    -alidns-key
-        阿里云AccessKeyID；用于访问阿里云API
-    -alidns-secret
-        阿里云AccessKeySecret；用于访问阿里云API
-    -alidns-domain
-        阿里云域名；需要更新的域名
-    -alidns-subdomain
-        阿里云子域名（如 www）
-    -alidns-ttl
-        阿里云TTL；域名解析记录的TTL值（默认 600）
-
-    -dnspod
-        启用DNSPod DNS更新；测速完成后将结果更新到DNSPod DNS；(默认 关闭)
-    -dnspod-secret-id
-        DNSPod Secret ID；用于访问DNSPod API
-    -dnspod-secret-key
-        DNSPod Secret Key；用于访问DNSPod API
-    -dnspod-domain
-        DNSPod域名；需要更新的域名
-    -dnspod-subdomain
-        DNSPod子域名（如 www）
-    -dnspod-ttl
-        DNSPod TTL；域名解析记录的TTL值（默认 600）
-
-    -cloudflare
-        启用Cloudflare DNS更新；测速完成后将结果更新到Cloudflare DNS；(默认 关闭)
-    -cf-token
-        Cloudflare API Token；用于访问Cloudflare API
-    -cf-zone
-        Cloudflare Zone ID；域名的Zone ID
-    -cf-domain
-        Cloudflare域名；需要更新的域名
-    -cf-subdomain
-        Cloudflare子域名（如 www）
-    -cf-proxy
-        是否开启Cloudflare代理；(默认 关闭)
-    -cf-ttl
-        Cloudflare TTL；域名解析记录的TTL值，1为自动（默认 1）
-
-    -cfkv
-        启用Cloudflare KV更新；测速完成后将结果更新到Cloudflare KV；(默认 关闭)
-    -cfkv-token
-        Cloudflare API Token；用于访问Cloudflare API
-    -cfkv-account
-        Cloudflare Account ID；账户ID
-    -cfkv-namespace
-        Cloudflare KV Namespace ID；KV命名空ID
-
+    -c config.toml
+        指定TOML配置文件；默认为config.toml，不存在时使用默认参数
     -debug
         调试输出模式；会在一些非预期情况下输出更多日志以便判断原因；(默认 关闭)
-
-    -c config.toml
-        指定TOML配置文件；指定配置文件时，全部参数从文件中读取，命令行参数无效
     -v
-        打印程序版本 + 检查版本更新
+        打印程序版本并检查版本更新
     -h
         打印帮助说明
 `
-	var minDelay, maxDelay, downloadTime int
-	var maxLossRate float64
-	flag.IntVar(&task.Routines, "n", 200, "延迟测速线程")
-	flag.IntVar(&task.PingTimes, "t", 4, "延迟测速次数")
-	flag.IntVar(&task.TestCount, "dn", 10, "下载测速数量")
-	flag.IntVar(&downloadTime, "dt", 10, "下载测速时间")
-	flag.IntVar(&task.TCPPort, "tp", 443, "指定测速端口")
-	flag.StringVar(&task.URL, "url", "https://cf.xiu2.xyz/url", "指定测速地址")
-
-	flag.BoolVar(&task.Httping, "httping", false, "切换测速模式")
-	flag.IntVar(&task.HttpingStatusCode, "httping-code", 0, "有效状态代码")
-	flag.StringVar(&task.HttpingCFColo, "cfcolo", "", "匹配指定地区")
-
-	flag.IntVar(&maxDelay, "tl", 9999, "平均延迟上限")
-	flag.IntVar(&minDelay, "tll", 0, "平均延迟下限")
-	flag.Float64Var(&maxLossRate, "tlr", 1, "丢包几率上限")
-	flag.Float64Var(&task.MinSpeed, "sl", 0, "下载速度下限")
-
-	flag.IntVar(&utils.PrintNum, "p", 10, "显示结果数量")
-	flag.StringVar(&task.IPv4File, "f4", "", "IPv4段数据文件")
-	flag.StringVar(&task.IPv6File, "f6", "", "IPv6段数据文件")
-	flag.StringVar(&task.IPFile, "f", "ip.txt", "IP段数据文件")
-	flag.StringVar(&task.IPText, "ip", "", "指定IP段数据")
-	flag.StringVar(&utils.Output, "o", "result.csv", "输出结果文件")
-
-	flag.BoolVar(&task.Disable, "dd", false, "禁用下载测速")
-	flag.BoolVar(&task.TestAll, "allip", false, "测速全部 IP")
-
-	flag.BoolVar(&enableAliDNS, "alidns", false, "启用阿里云DNS更新")
-	flag.StringVar(&ddns.AliDNSConfig.AccessKeyID, "alidns-key", "", "阿里云AccessKeyID")
-	flag.StringVar(&ddns.AliDNSConfig.AccessKeySecret, "alidns-secret", "", "阿里云AccessKeySecret")
-	flag.StringVar(&ddns.AliDNSConfig.Domain, "alidns-domain", "", "阿里云域名")
-	flag.StringVar(&ddns.AliDNSConfig.Subdomain, "alidns-subdomain", "", "阿里云子域名")
-	flag.IntVar(&ddns.AliDNSConfig.TTL, "alidns-ttl", 600, "阿里云TTL")
-
-	flag.BoolVar(&enableDNSPod, "dnspod", false, "启用DNSPod DNS更新")
-	flag.StringVar(&ddns.DNSPodConfig.SecretID, "dnspod-secret-id", "", "DNSPod Secret ID")
-	flag.StringVar(&ddns.DNSPodConfig.SecretKey, "dnspod-secret-key", "", "DNSPod Secret Key")
-	flag.StringVar(&ddns.DNSPodConfig.Domain, "dnspod-domain", "", "DNSPod域名")
-	flag.StringVar(&ddns.DNSPodConfig.Subdomain, "dnspod-subdomain", "", "DNSPod子域名")
-	flag.IntVar(&ddns.DNSPodConfig.TTL, "dnspod-ttl", 600, "DNSPod TTL")
-
-	flag.BoolVar(&enableCloudflare, "cloudflare", false, "启用Cloudflare DNS更新")
-	flag.StringVar(&ddns.CloudflareConfig.APIToken, "cf-token", "", "Cloudflare API Token")
-	flag.StringVar(&ddns.CloudflareConfig.ZoneID, "cf-zone", "", "Cloudflare Zone ID")
-	flag.StringVar(&ddns.CloudflareConfig.Domain, "cf-domain", "", "Cloudflare域名")
-	flag.StringVar(&ddns.CloudflareConfig.Subdomain, "cf-subdomain", "", "Cloudflare子域名")
-	flag.BoolVar(&ddns.CloudflareConfig.Proxied, "cf-proxy", false, "是否开启Cloudflare代理")
-	flag.IntVar(&ddns.CloudflareConfig.TTL, "cf-ttl", 1, "Cloudflare TTL（1为自动）")
-
-	flag.BoolVar(&enableCFKV, "cfkv", false, "启用Cloudflare KV更新")
-	flag.StringVar(&ddns.CloudflareKVConfig.APIToken, "cfkv-token", "", "Cloudflare API Token")
-	flag.StringVar(&ddns.CloudflareKVConfig.AccountID, "cfkv-account", "", "Cloudflare Account ID")
-	flag.StringVar(&ddns.CloudflareKVConfig.NamespaceID, "cfkv-namespace", "", "Cloudflare KV Namespace ID")
-
 	flag.BoolVar(&utils.Debug, "debug", false, "调试输出模式")
 	flag.StringVar(&configFile, "c", "", "指定TOML配置文件")
 	flag.BoolVar(&printVersion, "v", false, "打印程序版本")
 	flag.Usage = func() { fmt.Print(help) }
 	flag.Parse()
 
-	// 如果指定了配置文件，从配置文件加载参数
+	var config *Config
+	var err error
+
 	if configFile != "" {
-		config, err := LoadConfig(configFile)
+		// 如果指定了配置文件，则加载它
+		config, err = LoadConfig(configFile)
 		if err != nil {
 			fmt.Printf("加载配置文件失败: %v\n", err)
 			os.Exit(1)
 		}
-		ApplyConfig(config)
-		// 配置文件中的延迟参数需要特殊处理
-		if config.MaxDelay > 0 {
-			maxDelay = config.MaxDelay
-		}
-		if config.MinDelay >= 0 {
-			minDelay = config.MinDelay
-		}
-		if config.MaxLossRate >= 0 && config.MaxLossRate <= 1 {
-			maxLossRate = config.MaxLossRate
-		}
-		if config.DownloadTime > 0 {
-			downloadTime = config.DownloadTime
+	} else {
+		// 如果未指定配置文件，则尝试加载默认的 config.toml
+		config, err = LoadConfig("config.toml")
+		if err != nil {
+			// 如果加载失败，则使用默认配置
+			config = CreateDefaultConfig()
 		}
 	}
 
-	if task.MinSpeed > 0 && maxDelay == 9999 {
-		utils.Yellow.Println("[提示] 在使用 [-sl] 参数时，建议搭配 [-tl] 参数，以避免因凑不够 [-dn] 数量而一直测速...")
+	ApplyConfig(config)
+	
+	if task.MinSpeed > 0 && config.MaxDelay == 9999 {
+		utils.Yellow.Println("[提示] 配置了 min_speed 参数时，建议搭配 max_delay 参数，以避免因凑不够 test_count 数量而一直测速...")
 	}
-	utils.InputMaxDelay = time.Duration(maxDelay) * time.Millisecond
-	utils.InputMinDelay = time.Duration(minDelay) * time.Millisecond
-	utils.InputMaxLossRate = float32(maxLossRate)
-	task.Timeout = time.Duration(downloadTime) * time.Second
-	task.HttpingCFColomap = task.MapColoMap()
 
 	if printVersion {
 		println(version)
@@ -252,19 +92,12 @@ https://github.com/Lyxot/CloudflareSpeedTestDNS
 
 func main() {
 	fmt.Printf("# Lyxot/CloudflareSpeedTestDNS %s \n\n", version)
-
-	// 智能判断文件优先级
-	if task.IPv4File != "" || task.IPv6File != "" {
-		// 如果指定了f4或f6，则f参数无效
-		task.IPFile = ""
-	}
-
 	if enableCron {
 		cron() // 定时任务
 	} else {
 		speedTest() // 开始测速
-		endPrint() // 根据情况选择退出方式（针对 Windows）
 	}
+	endPrint() // 根据情况选择退出方式（针对 Windows）
 }
 
 func cron() {
@@ -280,6 +113,7 @@ func cron() {
 		case <-testTicker.C:
 			fmt.Println("\n强制刷新任务开始...")
 			ipData = speedTest()
+			checkTicker.Reset(checkInterval)
 		case <-checkTicker.C:
 			fmt.Println("\n开始检查延迟和丢包率...")
 			// 拼接 IP 段数据
